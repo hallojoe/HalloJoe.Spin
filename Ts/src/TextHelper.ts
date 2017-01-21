256ef0c1076a887e03b719d77a6808027c1bc30f
hallojoe <casper.korsgaard@outlook.com>
2017-01-21 23:33:26 +0100

    Remove empty values

--- Ts/src/TextHelper.ts
+++ Ts/src/TextHelper.ts
@@ -18,9 +18,18 @@ module SpinTexter
         /// <returns></returns>
         public static WordsCount(s: string)
         {
-            var pattern = "[" + this._seperators.join() + "]";
-            //console.log(pattern);
-            return s.split(new RegExp(pattern, "g")).length;
+            s = s.trim();
+            let pattern = "[" + this.escapeRegExp(this._seperators.join()) + "]";
+            let values = s.split(new RegExp(pattern, "g"));
+            let result = 0;
+            for (var i = 0; i < values.length; i++) 
+                if (values[i].trim() !== '')
+                    result += 1;
+            return result;
+        } 
+
+        private static escapeRegExp(s: string) : string {
+            return s.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
         }
     }
 }
